b7925052cf650eb3d387ddac8bb7526c175fdb80
Joao Gabriel <joao.almeida.gabriel2015@gmail.com>
2026-03-07 02:59:29 -0300

    chore(types): extend Express Request interface with user property

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -12,7 +12,7 @@ export default (req: Request, res: Response, next: NextFunction) => {
         const token = authHeader.split(" ")[1];
         if(!token) return res.status(401).json({ message: "Invalid authorization format!" });
 
-        (req as any).user = jwt.verify(token, SECRET_KEY);
+        req.user = jwt.verify(token, SECRET_KEY);
 
         next();
     } catch (error) {
